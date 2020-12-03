6b29fad35533e396691e8d5a3c494f7b2eb0b845
garrottkai <garrottkai@gmail.com>
2020-12-03 16:55:51 -0700

    added inputs to map

--- src/screens/Map.js
+++ src/screens/Map.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   SafeAreaView,
-  StyleSheet
+  StyleSheet,
+  View,
+  TextInput
 } from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+
+const Input = ({
+  placeholder,
+  style
+}) => {
+  const [value, onChangeText] = useState(placeholder);
+  return (
+    <TextInput
+      value={value}
+      onChangeText={onChangeText}
+      style={{
+        ...styles.input,
+        ...style
+      }}
+    />
+  )
+};
+
+const InputBox = () => {
+
+  return (
+    <View style={styles.inputBox}>
+      <Input placeholder='Start...' style={styles.topInput} />
+      <Input placeholder='End...' />
+    </View>
+  );
+};
 
 const Map = () => {
 
@@ -19,6 +48,7 @@ const Map = () => {
           longitudeDelta: 0.08,
         }}
       />
+      <InputBox />
     </SafeAreaView>
   );
 };
@@ -29,6 +59,27 @@ const styles = StyleSheet.create({
   },
   map: {
     flex: 1
+  },
+  inputBox: {
+    position: 'absolute',
+    bottom: 8,
+    left: 8,
+    right: 8,
+    backgroundColor: '#ffffff',
+    borderRadius: 4,
+    paddingVertical: 15,
+    paddingHorizontal: 15
+  },
+  input: {
+    borderRadius: 4,
+    borderColor: '#00000033',
+    borderWidth: 1,
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    fontSize: 18
+  },
+  topInput: {
+    marginBottom: 15
   }
 });
 
