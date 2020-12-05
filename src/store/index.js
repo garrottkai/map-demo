bcab923964375adddc1b5cb65ddd1ecfc2817c45
garrottkai <garrottkai@gmail.com>
2020-12-05 15:50:37 -0700

    scrapped sagas

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,6 @@
-import {
-  createStore,
-  applyMiddleware
-} from 'redux';
-import createSagaMiddleware from 'redux-saga';
+import { createStore } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import AsyncStorage from '@react-native-community/async-storage';
-import { allSagas } from './sagas';
 import reducer from './reducers';
 
 const persistConfig = {
@@ -13,12 +8,7 @@ const persistConfig = {
   storage: AsyncStorage
 };
 const persistedReducer = persistReducer(persistConfig, reducer);
-const sagaMiddleware = createSagaMiddleware();
-const finalCreateStore = applyMiddleware(
-  sagaMiddleware,
-)(createStore);
-const store = finalCreateStore(persistedReducer);
-sagaMiddleware.run(allSagas);
+const store = createStore(persistedReducer);
 const persistor = persistStore(store);
 
 export {
